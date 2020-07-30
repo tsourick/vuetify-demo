98f8291e845ab4a560d44db237563ca2c1334476
tsourick <tsourick@gmail.com>
2020-07-30 03:45:14 +0300

    style: cleanup

--- src/main.js
+++ src/main.js
@@ -2,18 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 
+// Use Utils as plugin
 // import Utils  from './plugins/utils.js';
 // Vue.use(Utils)
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-
 new Vue({
   vuetify,
-  render: h => h(App),
-  mounted (){
-    window.vueapp = this
-  }
+  render: h => h(App)
 }).$mount('#app')
